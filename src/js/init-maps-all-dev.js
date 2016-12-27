dc9c5a493e87283885b13cc3ed1eeebd8ddbb6cb
Zimbo0397 <zimbo0397@gmail.com>
2016-12-28 00:52:27 +0200

    +6 pages

---
+++ src/js/init-maps-all-dev.js
@@ -0,0 +1,80 @@
+
+var markersArr = [
+	{
+		lat: -34.297,
+		lng: 150.544,
+		icon: 'img/icons/pin-red.png',
+		content: '<p>2017-01-01 09:30:27 OUT Alert</p><p>Device left  its fence area.</p><p>Yancht: Princess Viking</p>'
+	},
+	{
+		lat: -34.397,
+		lng: 150.644,
+		icon: 'img/icons/pin-blue.png',
+		content: '<p>2017-01-01 09:30:27 OUT Alert</p><p>Device left  its fence area.</p><p>Yancht: Princess Viking</p>'
+	},
+	{
+		lat: -34.197,
+		lng: 150.344,
+		icon: 'img/icons/pin-red.png',
+		content: '<p>2017-01-01 09:30:27 OUT Alert</p><p>Device left  its fence area.</p><p>Yancht: Princess Viking</p>'
+	},
+	{
+		lat: -34.697,
+		lng: 150.844,
+		icon: 'img/icons/pin-blue.png',
+		content: '<p>2017-01-01 09:30:27 OUT Alert</p><p>Device left  its fence area.</p><p>Yancht: Princess Viking</p>'
+	},
+	{
+		lat: -34.997,
+		lng: 150.944,
+		icon: 'img/icons/pin-red.png',
+		content: '<p>2017-01-01 09:30:27 OUT Alert</p><p>Device left  its fence area.</p><p>Yancht: Princess Viking</p>'
+	}
+];
+
+var infoWindows = [],
+	markers = [];
+
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      scrollwheel: false,
+      zoom: 8
+    });
+
+
+
+	for(var i = 0; i < markersArr.length; i++) {
+
+		var content = markersArr[i].content;
+
+		var infowindow = new google.maps.InfoWindow({
+			content: content
+		});
+
+		infoWindows.push(infowindow);
+
+		var marker = new google.maps.Marker({
+		    position: {lat: markersArr[i].lat, lng: markersArr[i].lng},
+		    map: map,
+		    icon: markersArr[i].icon,
+		    title: 'marker'+[i]+''
+	  	});
+
+	  	markers.push(marker);
+
+	};
+
+	markers.map(function (marker, i) {
+
+		marker.addListener('click', function() {
+			infoWindows[i].open(map, marker);
+		});
+	});
+
+
+  }
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,37 @@
 
-if ($.browser.mobile) $('body').addClass('mobile');
-if ($.browser.safari) $('body').addClass('safari');
-if ($.browser.iphone || $.browser.ipad || $.browser.ipod ) $('body').addClass('ios');
-
-$(document).on('ready', function () {
-	var $window = $(window),
-		windowWidth = $window.width(),
-		windowHeight = $window.height(),
-		bodyHeight = $('body').height();
 
 
-		// resize
-		$window.on('resize', function () {
 
-			windowWidth = $window.width();
-			windowHeight = $window.height();
-			bodyHeight = $('body').height();
-
-		});
-
+$('tr[data-href]').on("click", function() {
+    document.location = $(this).data('href');
 });
 
 
+
+
+
 $(window).on('load', function() {
-	$('#scroll-btn').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+
+	$('.ui-piker').each(function() {
+		$(this).datepicker().datepicker("option", "dateFormat", "d M, y");
 	});
-})
 
+	$('.t-btn').each(function() {
+		var $self = $(this);
+		$self.on('click', function(e) {
+			e.preventDefault();
+			$self.closest('table').toggleClass('close');
 
-
-
-
-$(window).on('load', function() {
-	$('.iframe-holder').each(function() {
-
-		$(this).on('click', function() {
-
-			$self= $(this),
-			selfData = $self.attr('data-href');
-
-			$('.iframe-holder').removeClass('active').html('');
-			$self.addClass('active');
-
-			$self.html('<iframe id="ytVid"src="http://www.youtube.com/embed/'+selfData+'?enablejsapi=1" frameborder="0" allowfullscreen></iframe>');
-			
-			$('#ytVid').on('load', function() {
-				var iframe = document.getElementById("ytVid").contentWindow;
-				iframe.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-			})
+			if($self.text() == 'more...') {
+				$self.text('less...')
+			} else {
+				$self.text('more...')
+			}
 			
 		})
-
 	});
+
+
 	
 });
 
-
-$('#m-nav-btn').on('click', function() {
-	$('.fixed-panel').toggleClass('open')
-})
-
-// Blueimp Init
-if(document.getElementById('links')) {
-	document.getElementById('links').onclick = function (event) {
-	    event = event || window.event;
-	    var target = event.target || event.srcElement,
-	        link = target.src ? target.parentNode : target,
-	        options = {index: link, event: event},
-	        links = this.getElementsByTagName('a');
-	    blueimp.Gallery(links, options);
-	};
-}
